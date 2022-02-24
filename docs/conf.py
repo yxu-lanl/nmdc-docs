3d3a6319d376433de8d9812c1c08fe86f4d4abad
Yan Xu <yxu@pn1704854.lanl.gov>
2022-02-23 17:54:01 -0700

    updated

--- docs/conf.py
+++ docs/conf.py
@@ -64,6 +64,6 @@ html_static_path = ['_static']
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
-    '_static/css/custom.css',
+    'css/custom.css',
 ]
 html_logo = "_static/images/nmdc-logo.png"
